d21e4603b27fdaba1ec2232645e26942b21b1202
Shijin Mohanan Sheela <getshijin@gmail.com>
2024-09-05 14:00:22 +0530

    version3

--- etl_pipline.py
+++ etl_pipline.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 import pandas as pd
-
+import sqlite3
+import mysql.connector
+from mysql.connector import Error
 
 def extract_data():
     # Example: Extract data from a CSV file
@@ -10,16 +12,17 @@ def extract_data():
 # Transform
 def transform_data(data):
     # Example: Simple transformation
-    data['new_column'] = data['existing_column'] * 2
+    data['new_column'] = data['customer_id'] * 2
     return data
 
 # Load
 def load_data(data):
     # Example: Load data into a PostgreSQL database
-    engine = create_engine('postgresql://username:password@localhost:5432/demo')
-    data.to_sql('order', engine, if_exists='replace', index=False)
+    engine = create_engine('postgresql://postgres:root@localhost:5432/greencycle')
+    data.to_sql('orders', engine, if_exists='replace', index=False)
 
 if __name__ == "__main__":
     data = extract_data()
     data = transform_data(data)
     load_data(data)
+    print("finish")
--- test.py
+++ test.py
@@ -1,4 +1,35 @@
+import psycopg2
+from psycopg2 import OperationalError
 import pandas as pd
+
+def verify_postgresql_password(host, port, dbname, user, password):
+    try:
+        # Try to connect to the database
+        connection = psycopg2.connect(
+            host=host,
+            port=port,
+            dbname=dbname,
+            user=user,
+            password=password
+        )
+        # If the connection is successful, return True
+        connection.close()  # Close the connection
+        return True
+    except OperationalError as e:
+        # If the connection fails, return False
+        return False
+
+# Example usage
+host = "localhost"
+port = 5432
+dbname = "demo"
+user = "postgres"
+password = "password"
 
 a= pd.read_csv('data/orders.csv')
 print(a)
+
+if verify_postgresql_password(host, port, dbname, user, password):
+    print("Password is correct.")
+else:
+    print("Password is incorrect.")
